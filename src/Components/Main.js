d29e3cda68a2f4b216382df839d152c84fa8745d
Rajaram32 <rajaram321992@gmail.com>
2025-06-30 08:49:35 +0530

    moonlit

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import '../css/main.css'
 import MainPage from './MainPage'
-import { Route, Routes } from 'react-router-dom'
 import HeaderOne from './HeaderOne'
 import Header from './Header'
 
 const Main = () => {
     return (
         <>
-            {/* <Routes>
-                <Route path="/" element={}>
-                    <Route index element={} />
-                </Route>
-            </Routes> */}
             <HeaderOne />
             <Header />
             <MainPage />
